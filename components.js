eebe51c2489bf99d7c9d81cc7c2be1e7d05c664c
Ewan <135170229+xxmauricexx@users.noreply.github.com>
2026-03-21 18:04:54 +0000

    Add files via upload

---
+++ components.js
@@ -0,0 +1,7 @@
+document.querySelector('.marquee-bar').innerHTML = `
+    <marquee scrollamount="10">
+	  [ ⚠️ under construction ⚠️ ]
+      [ upcoming notes | ring theory, more linear algebra ]
+      [ last updated | 2025-03-21 ]
+    </marquee>
+`;
