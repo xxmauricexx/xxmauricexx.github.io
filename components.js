69f91ffc56ba36dd79803d4bc89544c2a2b7b80a
Ewan <135170229+xxmauricexx@users.noreply.github.com>
2026-03-21 18:15:21 +0000

    Update components.js

--- components.js
+++ components.js
@@ -2,6 +2,6 @@ document.querySelector('.marquee-bar').innerHTML = `
     <marquee scrollamount="10">
 	  [ ⚠️ under construction ⚠️ ]
       [ upcoming notes | ring theory, more linear algebra ]
-      [ last updated | 2025-03-21 ]
+      [ last updated | 2026-03-21 ]
     </marquee>
 `;
